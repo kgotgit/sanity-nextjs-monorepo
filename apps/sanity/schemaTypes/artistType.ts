caef8df26d93414cc77456a8c41a4baa48867493
Karthik Gotrala <6868408+kgotgit@users.noreply.github.com>
2026-07-14 05:24:15 -0500

    day 07/14 updates

--- apps/sanity/schemaTypes/artistType.ts
+++ apps/sanity/schemaTypes/artistType.ts
@@ -1,9 +1,11 @@
+import UsersIcon from '@sanity/icons/Users'
 import {defineField, defineType} from 'sanity'
 
 export const artistType = defineType({
   name: 'artist',
   title: 'Artist',
   type: 'document',
+  icon: UsersIcon,
   fields: [
     defineField({
       name: 'name',
--- apps/sanity/schemaTypes/eventType.ts
+++ apps/sanity/schemaTypes/eventType.ts
@@ -1,52 +1,123 @@
-import {defineField, defineType} from 'sanity'
+import { defineField, defineType } from 'sanity'
+import { CalendarIcon } from '@sanity/icons/Calendar'
+import { DoorsOpenInput } from './components/DoorsOpenInput'
 
 export const eventType = defineType({
-  name: 'event',
-  title: 'Event',
-  type: 'document',
-  fields: [
-    defineField({
-      name: 'name',
-      type: 'string',
-    }),
-    defineField({
-      name: 'slug',
-      type: 'slug',
-    }),
-    defineField({
-      name: 'eventType',
-      type: 'string',
-    }),    
-    defineField({
-      name: 'date',
-      type: 'datetime',
-    }),
-    defineField({
-      name: 'doorsOpen',
-      type: 'number',
-    }),
-    defineField({
-      name: 'venue',
-      type: 'reference',
-      to: [{type: 'venue'}],
-    }),
-    defineField({
-      name: 'headline',
-      type: 'reference',
-      to: [{type: 'artist'}],
-    }),
-    defineField({
-      name: 'image',
-      type: 'image',
-    }),
-    defineField({
-      name: 'details',
-      type: 'array',
-      of: [{type: 'block'}],
-    }),
-    defineField({
-      name: 'tickets',
-      type: 'url',
-    }),
-  ],
+    name: 'event',
+    title: 'Event',
+    type: 'document',
+    icon: CalendarIcon,
+    groups: [
+        { name: 'details', title: 'Details' },
+        { name: 'editorial', title: 'Editorial' },
+    ],
+
+    fields: [
+        defineField({
+            name: 'name',
+            type: 'string',
+            group: 'details',
+        }),
+        // Replace "slug" in the array of fields:
+        defineField({
+            name: 'slug',
+            type: 'slug',
+            options: { source: 'name' },
+            validation: (rule) => rule
+                .required()
+                .error(`Required to generate a page on the website`),
+            hidden: ({ document }) => !document?.name,
+            group: 'details',
+        }),
+        defineField({
+            name: 'eventType',
+            type: 'string',
+            options: {
+                list: ['in-person', 'virtual'],
+                layout: 'radio',
+            },
+            group: ['details', 'editorial'],
+        }),
+        defineField({
+            name: 'date',
+            type: 'datetime',
+            group: 'details',
+        }),
+        defineField({
+            name: 'doorsOpen',
+            description: 'Number of minutes before the start time for admission',
+            type: 'number',
+            initialValue: 60,
+            group: 'details',
+            components:{
+                input: DoorsOpenInput
+            }
+        }),
+        defineField({
+            name: 'venue',
+            type: 'reference',
+            to: [{ type: 'venue' }],
+            readOnly: ({ value, document }) => !value && document?.eventType === 'virtual',
+            validation: (rule) =>
+                rule.custom((value, context) => {
+                    if (value && context?.document?.eventType === 'virtual') {
+                        return 'Only in-person events can have a venue'
+                    }
+
+                    return true
+                }),
+            group: ['details', 'editorial'],
+        }),
+        defineField({
+            name: 'headline',
+            type: 'reference',
+            to: [{ type: 'artist' }],
+            group: 'editorial',
+        }),
+        defineField({
+            name: 'image',
+            type: 'image',
+            group: 'editorial',
+        }),
+        defineField({
+            name: 'details',
+            type: 'array',
+            of: [{ type: 'block' }],
+            group: 'editorial',
+        }),
+        defineField({
+            name: 'tickets',
+            type: 'url',
+            group: 'editorial',
+        }),
+    ],
+    // After the "fields" array
+    // Update the preview key in the schema
+preview: {
+  select: {
+    name: 'name',
+    venue: 'venue.name',
+    artist: 'headline.name',
+    date: 'date',
+    image: 'image',
+  },
+  prepare({name, venue, artist, date, image}) {
+    const nameFormatted = name || 'Untitled event'
+    const dateFormatted = date
+      ? new Date(date).toLocaleDateString(undefined, {
+          month: 'short',
+          day: 'numeric',
+          year: 'numeric',
+          hour: 'numeric',
+          minute: 'numeric',
+        })
+      : ''
+
+    return {
+      title: artist ? `${nameFormatted} (${artist})` : nameFormatted,
+      subtitle: venue ? `${dateFormatted} @ ${venue}` : dateFormatted,
+      media: image || CalendarIcon,
+    }
+  },
+},
 })
